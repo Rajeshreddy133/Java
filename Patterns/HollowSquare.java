36752764bb37d7723ffdf6549bed18e811d67bce
Rajesh Reddy <244g1a05v9@srit.ac.in>
2026-01-10 12:25:40 +0530

    Update and rename Square.java to HollowSquare.java

--- Patterns/HollowSquare.java
+++ Patterns/HollowSquare.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class Square{
+class HollowSquare{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
@@ -20,4 +20,5 @@ class Square{
             System.out.println("");
         }
     }
+
 }
