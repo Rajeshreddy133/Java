1bca23b5b9ee5f81306a8f6315012af26243ceee
Rajesh Reddy <244g1a05v9@srit.ac.in>
2026-02-24 19:14:41 +0530

    Added New Files To Find Reverse Number

---
+++ Numbers/ReverseNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.*;
+class ReverseNumber{
+    public static int reverse(int x) {
+        int rev=0;
+        while(x!=0){
+            int rem=x%10;
+            x/=10;
+            if(rev>Integer.MAX_VALUE/10 || 
+              (rev== Integer.MAX_VALUE/10 && rem>7)){
+                    return 0;
+                }
+            if(rev<Integer.MIN_VALUE/10 ||
+              (rev==Integer.MIN_VALUE/10 && rem<-8)){
+                  return 0;
+            }
+            rev=rev*10+rem;
+        }
+        return rev;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println(reverse(num));
+    }
+}
