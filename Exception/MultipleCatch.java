d51f289a67bae9911bd1e68426bee60014029379
Rajesh Reddy <244g1a05v9@srit.ac.in>
2026-03-07 16:07:37 +0530

    Added Multiple Catch File

---
+++ Exception/MultipleCatch.java
@@ -0,0 +1,21 @@
+public class MultipleCatch{
+    public static void main(String[] args){  
+        try{
+            int a = 10 / 0;
+            int arr[] = new int[5];
+            arr[10] = 50;
+        }
+        catch (ArithmeticException e) {
+            System.out.println(
+                "Arithmetic Exception occurred");
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(
+                "Array Index Out Of Bounds Exception occurred");
+        }
+        catch (Exception e) {
+            System.out.println(
+                "General Exception occurred");
+        }
+    }
+}
