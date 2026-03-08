8920fe9b534cdef0a14ccd99e92ad2cb5771aded
Rajesh Reddy <244g1a05v9@srit.ac.in>
2026-03-08 14:03:11 +0530

    Added Generic Class File

---
+++ Collections/GenericClass.java
@@ -0,0 +1,17 @@
+
+class Test<T> {
+    T obj;
+    Test(T obj) { 
+        this.obj = obj;
+    }
+    public T getObject() { return this.obj; }
+}
+class GenericClass{
+    public static void main(String[] args)
+    {
+        Test<Integer> iObj = new Test<Integer>(15);
+        System.out.println(iObj.getObject());
+        Test<String> sObj= new Test<String>("Hi EveryOne");
+        System.out.println(sObj.getObject());
+    }
+}
