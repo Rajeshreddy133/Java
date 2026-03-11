0ef8e349619e380a5a3bb691ff298ea4f47d129f
Rajesh Reddy <244g1a05v9@srit.ac.in>
2026-03-11 17:48:43 +0530

    Added LowerBounded WildCards File

--- Collections/UpperBoundedWildCards.java
+++ Collections/UpperBoundedWildCards.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
-class Wildcards{
+class UpperBoundedWildCards{
     public static void main(String[] args)
     {
         List<Integer> list1 = Arrays.asList(4, 5, 6, 7);
