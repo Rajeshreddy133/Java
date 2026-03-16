6393ff3d267e05fc20e635c072b1dcbc97b4b6b9
Rajesh Reddy <244g1a05v9@srit.ac.in>
2026-03-16 14:48:01 +0530

    Added Lambda Expressions File

---
+++ Collections/LambdaExpressions.java
@@ -0,0 +1,10 @@
+interface Add{
+    int addition(int a, int b);
+}
+public class LambdaExpressions{ 
+    public static void main(String[] args){ 
+        Add add = (a, b) -> a + b;
+        int result = add.addition(10, 20);
+        System.out.println("Sum: " + result);
+    }
+} 
