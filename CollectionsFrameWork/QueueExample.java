1d6ebb68f34a5f01de9ce89c7239f948dabfd64d
Rajesh Reddy <244g1a05v9@srit.ac.in>
2026-05-14 21:00:16 +0530

    Added QueueExample File

---
+++ CollectionsFrameWork/QueueExample.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class QueueExample{
+    public static void main(String args[])
+    {
+        Queue<String> pq = new PriorityQueue<>();
+        pq.add("Geeks");
+        pq.add("For");
+        pq.add("Geeks");
+        System.out.println("Initial Queue: " + pq);
+        pq.remove("Geeks");
+        System.out.println("After Remove: " + pq);
+        System.out.println("Poll Method: " + pq.poll());
+        System.out.println("Final Queue: " + pq);
+    }
+}
